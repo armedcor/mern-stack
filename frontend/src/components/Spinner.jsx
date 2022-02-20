50aa48270477ab386f7153fedff8a7d187b9d771
Cormac Lawlor <51192198+armedcor@users.noreply.github.com>
2022-02-20 12:18:55 +0000

    Hooked up auth backend to frontend UI

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Spinner() {
+  return (
+    <div className="loadingSpinnerContainer">
+      <div className="loadSpinner"></div>
+    </div>
+  );
+}
+
+export default Spinner;
